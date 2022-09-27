4555f2a62525e2586c948309cf5b8867f3f418e7
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-27 11:56:05 -0400

    Added methods to Snake class for moving in each direction.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """This app allows the user to play Snake using the `turtle` library."""
 import turtle as t
 import time
-#from helpers import turn_left
+from snake import Snake
 
 # Create a black screen using Screen class.
 screen = t.Screen()
@@ -10,38 +10,24 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-# Create a snake body using Turtle class.
-# stretch = 3
-# segment = t.Turtle(shape="square")
-# segment.color("white")
-# segment.turtlesize(stretch_len=stretch)
+# Create a three segment Snake.
+snake = Snake()
 
-# starting_position = [(0, 0), (-20, 0), (-40, 0)]
-
-# segments = []
-
-for pos in starting_position:
-    new_segment = t.Turtle("square")
-    new_segment.color("white")
-    new_segment.pu()
-    new_segment.goto(pos)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 # Move the snake.
 game_is_on = True
 while game_is_on:
     screen.update()
-    # segment.fd(20)
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 # TODO Control the snake.
-screen.listen()
-screen.onkey()
+
 
 # TODO Detect collision with food.
 
--- snake.py
+++ snake.py
@@ -1,13 +1,48 @@
+"""Snake Class
+
+This script creates a Snake class for the Snake Game.
+This script requires `turtle` module to be imported.
+
+This script can be imported as a module containing the Snake class.
+"""
 import turtle as t
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 
 class Snake():
+    """
+    A class used to represent a Snake in the Snake Game
+    ...
+    Attributes
+    __________
+    segments: list of Turtle class squares
+    
+    Methods
+    _______
+    create_snake()
+        Creates a snake made of 3 square segments from Turtle class
+    move()
+        Starts moving the snake toward the right at a constant distance
+    up()
+        Turns the snake up - 90 degrees
+    down()
+        Turns the snake down - 270 degrees
+    left()
+        Turns the snake left - 180 degrees
+    right()
+        Turns the snake right - 0 degrees
+    """
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
     
     def create_snake(self):
+        """Creates a snake with 3 white square segments from Turtle class.
+        Uses the STARTING_POSITION constant in order to space segments.
+        Places segments in the correct position.
+        """
         for pos in STARTING_POSITIONS:
             new_segment = t.Turtle("square")
             new_segment.color("white")
@@ -16,8 +51,27 @@ class Snake():
             self.segments.append(new_segment)
     
     def move(self):
+        """Moves the snake at a constant MOVE_DISTANCE.
+        Takes into consideration that the individual segments need to trail behind eachother.
+        """
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+    
+    def up(self):
+        """Turns the snake up - 90 degrees"""
+        self.head.setheading(90)
+    
+    def down(self):
+        """Turns the snake down - 270 degrees"""
+        self.head.setheading(270)
+    
+    def left(self):
+        """Turns the snake left - 180 degrees"""
+        self.head.setheading(180)
+    
+    def right(self):
+        """Turns the snake right - 0 degrees"""
+        self.head.setheading(0)
