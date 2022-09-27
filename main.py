102457001e4fdead83487cfa67b6cdb858e5c1b2
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-27 10:51:46 -0400

    Created TODO list of functionality that needs to be added.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""This app allows the user to play Snake using the `turtle` library."""
+import turtle as t
+
+# TODO Create a snake body.
+
+# TODO Move the snake.
+
+# TODO Control the snake.
+
+# TODO Detect collision with food.
+
+# TODO Create a scoreboard.
+
+# TODO Detect collision with wall.
+
+# TODO Detect collision with tail.
