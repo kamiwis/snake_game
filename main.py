4b37eddebfbeeabcc27b111275fef86c6fa2dbab
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-27 10:56:35 -0400

    Created screen using Screen class.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 """This app allows the user to play Snake using the `turtle` library."""
 import turtle as t
+import random
 
 # TODO Create a snake body.
+# Create a black screen. 
+screen = t.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
 
 # TODO Move the snake.
 
@@ -14,3 +20,5 @@ import turtle as t
 # TODO Detect collision with wall.
 
 # TODO Detect collision with tail.
+
+screen.exitonclick()
