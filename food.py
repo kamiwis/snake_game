c7be73c2224f66fd1e45cde6673a962524f05dac
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-27 12:19:55 -0400

    Added refresh method to Food class so that food regenerates in a random location when a collision occurs.

--- food.py
+++ food.py
@@ -27,6 +27,10 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh()
+    
+    def refresh(self):
+        """Generates a new food at a random (x, y) location"""
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -28,7 +28,9 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-# TODO Detect collision with food.
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 # TODO Create a scoreboard.
 
